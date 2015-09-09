90323db446ba403c0487f644a3f104cc618ee331
betsylouj <Guest@wts6.attlocal.net>
2015-09-08 17:01:08 -0700

    5newPrograms

---
+++ levle_0/src/level_0/knockKnock.java
@@ -0,0 +1,11 @@
+package level_0;
+
+import javax.swing.JOptionPane;
+
+public class knockKnock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "How did the robot open the door");
+	JOptionPane.showMessageDialog(null, "he didn't he just fell over");
+	
+}
+}
